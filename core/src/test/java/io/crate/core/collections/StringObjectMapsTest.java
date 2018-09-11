94a6bafb99c27d23171269e54041936b881b1e70
Mathias Fussenegger <f.mathias@zignar.net>
2018-09-11 14:21:56 +0200

    WIP: Extract source generation logic for update operations

--- core/src/test/java/io/crate/core/collections/StringObjectMapsTest.java
+++ core/src/test/java/io/crate/core/collections/StringObjectMapsTest.java
@@ -24,6 +24,7 @@ package io.crate.core.collections;
 import io.crate.test.integration.CrateUnitTest;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -51,6 +52,14 @@ public class StringObjectMapsTest extends CrateUnitTest {
         assertNull(StringObjectMaps.getByPath(map, "a.b.c"));
         assertNull(StringObjectMaps.getByPath(map, "a.c"));
         assertNull(StringObjectMaps.getByPath(map, "b.c"));
+    }
 
+    @Test
+    public void testMergeIntoSourceWithNullEntry() {
+        HashMap<String, Object> m = new HashMap<>();
+        m.put("o", null);
+
+        expectedException.expectMessage("Object o is null, cannot write x = 10 into it");
+        StringObjectMaps.mergeInto(m, "o", Collections.singletonList("x"), 10);
     }
 }
